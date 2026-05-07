2153231ae6997e987ed8e1545c960e815c4d8d53
Santiago Salvador <salvsantiago@gmail.com>
2026-05-07 13:16:16 -0700

    Initial commit: charango tuner & Andean folkloric metronome with 8 rhythm presets, pendulum visual, and PWA support

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'charango-tuner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
